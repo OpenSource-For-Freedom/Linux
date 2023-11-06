a7fbeaedde1a17dbcba97a37878b85d94f86b4f9
HESC0 <92951498+OpenSource-For-Freedom@users.noreply.github.com>
2023-11-06 18:45:47 -0500

    Update sniffMe.py
    Added dpkt, schedule, user choice to automate weekly, decided to just go with linux file path.

--- sniffMe.py
+++ sniffMe.py
@@ -2,11 +2,15 @@ import os
 import nmap
 import csv
 import time
+import dpkt
 from pythonping import ping
+import schedule
 
 # Constants
 TARGET_IP = "192.168.1.254"
-NMAP_PATH = "C:\\Program Files (x86)\\Nmap\\nmap.exe"
+# Linux File Path - using a linux only file path for now
+OUTPUT_DIRECTORY = "/home/54321/Desktop/scans"
+# NMAP_PATH = "/usr/bin/nmap"
 OUTPUT_DIRECTORY = os.path.expanduser("~/Desktop/nmap_scans")
 
 # Function to ping the target IP address
@@ -25,7 +29,7 @@ def ping_target(ip_address):
 def ask_user_confirmation():
     while True:
         try:
-            user_input = int(input("Target is active. Do you want to proceed? (1 = Yes, 2 = No): "))
+            user_input = int(input("Target Aquired. Do you want to proceed? (1 = Yes, 2 = No): "))
             if user_input == 1:
                 return True
             elif user_input == 2:
@@ -35,7 +39,7 @@ def ask_user_confirmation():
         except ValueError:
             print("Invalid input. Please enter 1 to proceed or 2 to abort.")
 
-# Function to run an Nmap scan and handle errors
+# Function to run Nmap and handle errors
 def run_nmap_scan(nm, output_filename, scan_args):
     try:
         nm.scan(arguments=scan_args)
@@ -55,53 +59,84 @@ def run_nmap_scan(nm, output_filename, scan_args):
     except Exception as e:
         print(f"An error occurred during the Nmap scan for {output_filename}: {e}")
 
-# Check if the target is reachable by pinging it
-if not ping_target(TARGET_IP):
-    print(f"Terminating script because target IP {TARGET_IP} is not reachable.")
-else:
-    # Ask the user for confirmation to proceed
-    if ask_user_confirmation():
-        # Run each scan and save results to CSV files
-        scan_commands = [
-            "-sS -sU -T4 -A -v -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 --script \"default or (discovery and safe)\"",
-            "-p 1-65535 -T4 -A -v",
-            "-T4 -A -v -Pn",
-            "-sS -sU -T4 -A -v",
-            "-T4 -F"
-        ]
+# Function to capture and analyze network packets using dpkt
+def analyze_packets():
+    # Add code to analyze network packets here
+    
+    # we can open a pcap file and iterate through the packets
+    packets_data = []  # Placeholder for packet analysis data
+   # add script here
 
-        output_filenames = [
-            "slow_and_steady.csv",
-            "intense_tcp.csv",
-            "intense_ping.csv",
-            "udp.csv",
-            "quick.csv"
-        ]
+    return packets_data #needs to output on seperate CSV file
 
-        for i in range(len(scan_commands)):
-            try:
-                nm = nmap.PortScanner(nmap_search_path=NMAP_PATH)
-                scan_args = f"{TARGET_IP} {scan_commands[i]}"
-                run_nmap_scan(nm, output_filenames[i], scan_args)
-                
-                # Sleep until the scan is completed (alternative to fixed sleep)
-                while nm.still_scanning():
-                    time.sleep(1)
-            
-            except Exception as e:
-                print(f"An error occurred during the Nmap scan for {output_filenames[i]}: {e}")
-        
-        # Sleep for 5 seconds before saving the final CSV file
-        time.sleep(5)
-        
-        # Capture and analyze network packets using dpkt
-        # Add code for analyzing packets here (define analyze_packets function)
-        
-        # Save analysis results to a separate CSV file
-        # Add code for saving analysis to CSV here (define save_analysis_to_csv function)
-        
-        print("Scans completed, and results saved to 'nmap_scans' directory on the desktop.")
-        # Print a message about packet analysis
-        
+# Function to save analysis results to a separate CSV file
+def save_analysis_to_csv(data, filename):
+    # Add code to save analysis results to a CSV file
+    try:
+        full_output_path = os.path.join(OUTPUT_DIRECTORY, filename)
+        with open(full_output_path, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(["Source IP", "Destination IP"])  # Writing header
+            for row in data:
+                csv_writer.writerow(row)  # Writing each row of data
+        print(f"Analysis results saved to {filename}.")
+    except Exception as e:
+        print(f"An error occurred while saving analysis results to {filename}: {e}")
+
+# Function to automate the script to run weekly
+def run_weekly_scan():
+    if not ping_target(TARGET_IP):
+        print(f"Terminating script because target IP {TARGET_IP} is not reachable.")
     else:
-        print("Aborted by user.")
+        # Run each scan and save results to CSV files
+      
+
+        # Sleep for 5 seconds before capturing and analyzing network packets
+        time.sleep(5)
+
+        # Capture and analyze network packets using dpkt
+        packet_analysis_data = analyze_packets()
+
+        # Save analysis results to a separate CSV file
+        save_analysis_to_csv(packet_analysis_data, 'packet_analysis.csv')
+
+        print("Scans completed, and results saved to 'nmap_scans' directory on the desktop.")
+
+# Function to set file permissions for Linux systems
+def set_file_permissions(file_path, permission_code):
+    try:
+        os.chmod(file_path, permission_code)
+        print(f"File permissions for {file_path} set to {permission_code}.")
+    except Exception as e:
+        print(f"An error occurred while setting file permissions: {e}")
+
+# Function to check if the target is reachable by pinging it
+def ping_target(ip_address):
+    try:
+        response = ping(ip_address, count=4)  # Send 4 ping packets
+        if response.success():
+            return True
+        else:
+            raise Exception(f"Target IP {ip_address} is not reachable.")
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+
+# Function to ask the user for confirmation to proceed
+def ask_user_confirmation():
+    while True:
+        try:
+            user_input = int(input("Target Aquired. Do you want to proceed? (1 = Yes, 2 = No): "))
+            if user_input == 1:
+                return True
+            elif user_input == 2:
+                return False
+            else:
+                print("Invalid input. Please enter 1 to proceed or 2 to abort.")
+        except ValueError:
+            print("Invalid input. Please enter 1 to proceed or 2 to abort.")
+
+
+# Run the first scan and prompt the user for automation
+if ask_user_confirmation():
+    run
