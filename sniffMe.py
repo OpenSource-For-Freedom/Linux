e1d3798db9ac0be2a4e42a856525e368fa18737e
Tank <s3c@sudomail.com>
2024-02-24 17:48:57 +0000

    Added function space to import dependencies based on OS. Added space for OS function sensing path. Need to build OS_sense function and string request user input for OUTPUT_DIRECTORY

--- sniffMe.py
+++ sniffMe.py
@@ -8,8 +8,12 @@ import schedule
 
 # Constants
 TARGET_IP = "192.168.1.254"
-# Linux File Path - using a linux only file path for now
+# detect OS
+# build function ti detect OS based on imported directories and needed dependancies
+# Linux File Path 
 OUTPUT_DIRECTORY = "/home/54321/Desktop/scans"
+# Windows File path 
+OUTPUT_DIRECTORY = "FILE PATH TO WINDOWS DIRECTORY"
 # NMAP_PATH = "/usr/bin/nmap"
 OUTPUT_DIRECTORY = os.path.expanduser("~/Desktop/nmap_scans")
 
