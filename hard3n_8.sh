b2e63fc9dc84577716509e76898a64f5529a2a13
kiukiucat <152515561+kiukiucat@users.noreply.github.com>
2024-02-28 08:31:21 -0600

    Update hard3n_8.sh
    Small changes. Up ahead I need to add sysctl hardening, /proc/cmdline options for hardening as well, few more things. This is going to be for Debian 12 primarily?

--- hard3n_8.sh
+++ hard3n_8.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 # this package isntall all supported open source Linux hardening tools.
 # Update package lists
-sudo apt-get update
+sudo apt update
 
 # Function to check if a package is installed
 is_package_installed() {
     dpkg -l "$1" | grep -q "^ii"
 
 # Update package lists
-sudo apt-get update
+sudo apt update
 
 # Install ClamAV if not installed
 if ! is_package_installed clamav; then
-    sudo apt-get install -y clamav
+    sudo apt install -yy clamav --no-install-recommends --no-install-suggests
 fi
 
 # Install rkhunter if not installed
 if ! is_package_installed rkhunter; then
-    sudo apt-get install -y rkhunter
+    sudo apt install -yy rkhunter --no-install-recommends --no-install-suggests
 fi
 
 # Install chkrootkit if not installed
 if ! is_package_installed chkrootkit; then
-    sudo apt-get install -y chkrootkit
+    sudo apt install -yy chkrootkit --no-install-recommends --no-install-suggests
 fi
 
 # Install Fail2Ban if not installed
 if ! is_package_installed fail2ban; then
-    sudo apt-get install -y fail2ban
+    sudo apt install -yy fail2ban --no-install-recommends --no-install-suggests
 fi
 
 # Install Lynis if not installed
 if ! is_package_installed lynis; then
-    sudo apt-get install -y lynis
+    sudo apt install -yy lynis --no-install-recommends --no-install-suggests
 fi
 
 # Install AIDE if not installed
 if ! is_package_installed aide; then
-    sudo apt-get install -y aide
+    sudo apt install -yy aide --no-install-recommends --no-install-suggests
 fi
 
 # Install AppArmor if not installed
 if ! is_package_installed apparmor; then
-    sudo apt-get install -y apparmor
+    sudo apt-get install -yy apparmor apparmor-profiles apparmor-profiles-extra apparmor-utils --no-install-recommends --no-install-suggests
 
 
 echo "Security Tools Installed Successfully."
